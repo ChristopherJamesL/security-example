4a6ac73e1199cd29c9a89b8763e395f0d859035e
ChristopherL <anbchris@gmail.com>
2025-06-20 20:56:39 -0400

    begin implementing google oauth authentication with passport.

--- server.js
+++ server.js
@@ -3,18 +3,41 @@ const https = require('https');
 const path = require('node:path');
 const express = require('express');
 const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+
+require('dotenv').config();
 
 const PORT = 3000;
 const app = express();
 
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
+const AUTH_OPTIONS = {
+    callbackURL: 'auth/google/callback',
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+    console.log('Google Profile: ', profile);
+    done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
+
 const options = {
     key: fs.readFileSync('./key.pem'),
     cert: fs.readFileSync('./cert.pem'),
-}
+};
 
 const server = https.createServer(options, app);
 
 app.use(helmet());
+app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
     const isLoggedIn = true; // TODO
