55b0976ea75b88a1b4badd416b2fc9d0eb69474c
ChristopherL <anbchris@gmail.com>
2025-06-23 19:49:09 -0400

    begin implementing cookie sessions with express

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const helmet = require('helmet');
 const passport = require('passport');
 const { Strategy } = require('passport-google-oauth20');
+const cookieSession = require('cookie-session');
 
 require('dotenv').config();
 
@@ -14,6 +15,8 @@ const app = express();
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const AUTH_OPTIONS = {
@@ -37,6 +40,26 @@ const options = {
 const server = https.createServer(options, app);
 
 app.use(helmet());
+
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [ config.COOKIE_KEY_1, config.COOKIE_KEY_2 ],
+}));
+app.use((req, res, next) => {
+    if (req.session && !req.session.regenerate) {
+        req.session.regenerate = (cb) => {
+            cb();
+        };
+    }
+    if (req.session && !req.session.save) {
+        req.session.save = (cb) => {
+            cb();
+        };
+    }
+    next();
+});
+
 app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
